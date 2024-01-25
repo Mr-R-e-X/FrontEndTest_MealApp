a682f8c7fb3fff9206f0fe40e96a13b03badcf80
M-r-ReX <souvikhazra151@gmail.com>
2024-01-25 10:40:03 +0530

    randome meal deatails &filter by popular ingredients updated

--- src/script.js
+++ src/script.js
@@ -21,10 +21,10 @@ async function randomeMealData(){
                 let mealCard = document.createElement('div');
                 mealCard.classList.add('mb-5')
                 mealCard.innerHTML =  `
-                <div class="w-full max-w-72 bg-slate-900 rounded-lg m-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-lg hover:shadow-white">
-                        <img class="p-3 rounded" src=${meal.strMealThumb} alt="meal image" />
-                    <div class="px-1 pb-2">
-                            <h5 class="text-xl text-center font-semibold tracking-tight text-neutral-400 transition duration-300 ease-in-out hover:text-white"> ${meal.strMeal} </h5>
+                <div class="w-full max-w-60 rounded-lg m-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-lg hover:shadow-white" data='${meal.strMeal}' >
+                        <img class="p-3 rounded" src=${meal.strMealThumb} alt="meal image" data='${meal.strMeal}'/>
+                    <div class="px-1 pb-2" data='${meal.strMeal}'>
+                            <h5 class="text-lg text-center font-semibold tracking-tight text-neutral-400 transition duration-300 ease-in-out hover:text-white" data='${meal.strMeal}'> ${meal.strMeal} </h5>
                     </div>
                 </div>
                 `
@@ -35,56 +35,37 @@ async function randomeMealData(){
         console.log(error);
     }
 }
+document.addEventListener('click', (e)=>{
+    console.log(e.target);
+    let elem = e.target;
+    let mealName = elem.getAttribute('data');
+    if(mealName !== null){
+        console.log(mealName);
+        handleClickMealCard(mealName);
+    }
+})
+async function handleClickMealCard(mealName){
+    // console.log(mealName)
+    let searchByName_url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + mealName;
+    console.log(searchByName_url)
+    let result = await fetch(searchByName_url);
+    let data = await result.json();
+    let recipe = JSON.stringify(data);
+    localStorage.setItem('recipe', recipe);
+    localStorage.setItem('recipeName', mealName);
+    window.location.href = 'recipe.html';
+}
 
-// Search By Popular Infgredients
-async function SearchByIngredientsData(ingredients) {
+async function SearchByIngredientsData(ingredients){
     const url = searchByIngredients_url + ingredients;
     const result = await fetch(url);
     const data = await result.json();
-    console.log(data);
-
-    // Create the main ingredientsDiv container
-    let mainDiv = document.createElement('div')
-    mainDiv.classList.add('popup-container', 'bg-slate-800', 'text-neutral-400', 'font-Ubuntu', 'fadeIn')
-    let title = document.createElement('h1');
-    title.classList.add('transition-all', 'duration-300', 'ease-in-out', 'hover:text-white')
-    title.innerHTML = `The Meals You can get with ${ingredients}`;
-    title.classList.add('text-4xl','mb-5', 'text-center', 'p-3')
-    let closingButton = document.createElement('button');
-    closingButton.innerHTML = `Close`;
-    closingButton.classList.add('closing-button', 'bg-red-600', 'p-3', 'border-2', 'border-black', 'text-base');
-    mainDiv.append(title);
-    mainDiv.append(closingButton)
-    DOCBody.appendChild(mainDiv)
-    let ingredientsDiv = document.createElement('table');
-    ingredientsDiv.classList.add('flex', 'flex-row', 'flex-wrap', 'justify-center')
-    data.meals.forEach((meal) => {
-        let ingredientCard = document.createElement('div');
-        ingredientCard.innerHTML = `
-        <tr>
-            <div class="w-full max-w-72 bg-slate-900 rounded-lg m-2 transition duration-300 ease-in-out hover:bg-black hover:shadow-lg hover:shadow-white " data=${meal.strMeal}>
-                <a href="#" class="rounded">
-                    <img class="p-3 rounded" src=${meal.strMealThumb} alt="meal image" />
-                </a>
-                <div class="px-1 pb-2">
-                    <a href="#">
-                        <h5 class="text-xl text-center font-semibold tracking-tight text-neutral-400 transition duration-300 ease-in-out hover:text-white"> ${meal.strMeal} </h5>
-                    </a>
-                </div>
-            </div>
-        </tr>
-        `;
-        ingredientsDiv.appendChild(ingredientCard);
-    });
-    mainDiv.append(ingredientsDiv);
-    closingButton.addEventListener('click', (e) => {
-        mainDiv.classList.remove('fadeIn');
-        mainDiv.classList.add('fadeOut');
-        setTimeout(() => {
-            mainDiv.remove();
-        }, 500);
-    });
+    const dataAsString = JSON.stringify(data);
+    localStorage.setItem('searchByIngredients', dataAsString);
+    localStorage.setItem('ingredient', ingredients);
+    window.location.href = 'page2.html';
 }
+
 // calling the Fecth functions
 
 for(let i = 1; i <= 10; i++){
